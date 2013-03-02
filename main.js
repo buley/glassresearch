3e3d39aa92942591080aa9755265032ed41af27e
Taylor Buley <taylor.buley@gmail.com>
2013-03-02 12:55:31 -0500

    .

--- main.js
+++ main.js
@@ -10,21 +10,72 @@ fs.readFile( './.gplusapi', function ( err, data ) {
 		throw err;
 	}
 	var key = data.toString();
+	var path = '/plus/v1/activities?key=AIzaSyBaTMsCBdMDhaqhhc15eQzhpeGy5ydssXI&query=ifihadglass';
 	//console.log( key );
 	var request = https.request( {
 		host: 'www.googleapis.com'
 		, port: 443
-		, method: 'GET'
-		, path: '/plus/v1/activities?key=' + key + '
-		//, path: '/plus/v1/activities?key=' + key + '&query=ifihadglass'
+		, path: path
 	} ).on( 'response', function( response ) {
 		var body = '';
 		response.on( 'data', function( chunk ) {		
-			body += chunk.toString();
+			body += chunk;
 		} );	
-		response.on( 'close', function( ) {
-			console.log( body );
-		} );
+		response.on( 'end', function( ) {		
+			parseActivities( body.toString() );
+		} );	
 	 } );
 	 request.end();
 } );
+
+function saveNextToken( token ) {
+	fs.writeFile("./.gplustoken", token, function(err) {
+		if( err ) {
+			console.log( err );
+		} else {
+			console.log("Token saved");
+		}
+	});
+}
+
+function readNextToken( token, callback ) {
+	fs.readFile("./.gplustoken", token, function( err, data ) {
+		if( err ) {
+			console.log( err );
+			if ( 'function' is typeof callback ) {
+				callback( null );
+			}
+		} else {
+			//console.log( "Token", data );
+			if ( 'function' is typeof callback ) {
+				callback( data );
+			}
+		}
+	});
+}
+
+
+function parseActivities( body ) {
+	var response = JSON.parse( body );
+	if ( 'undefined' !== typeof response.nextPageToken ) {
+		saveNextToken( response.nextPageToken );
+	}
+	items = response.items;
+	if ( 'undefined' !== typeof items ) {
+		var x = 0, length = items.length, item;
+		for ( ; x < length ; x += 1 ) {
+			item = items[ x ];
+			writeActivity( item );
+		}
+	}
+}
+function writeActivity( object ) {
+	id = object.id;
+	fs.writeFile("./data/google/" + id, JSON.stringify( object ), function(err) {
+		if( err ) {
+			console.log( err );
+		} else {
+			console.log("Activity written");
+		}
+	});
+}
